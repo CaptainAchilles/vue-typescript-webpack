4fbe4ca0f8c991f6df671cf77bb968ce3789eda2
Nicholas Achilles <s4317811@student.uq.edu.au>
2017-04-02 15:06:44 +1000

    Moved the app container creation into main.ts

--- template/src/main.ts
+++ template/src/main.ts
@@ -3,6 +3,10 @@ import App from './App'
 
 import './styles/main.css'
 
+var appContainer = document.createElement("div");
+appContainer.id = "app";
+
+document.body.appendChild(appContainer);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
